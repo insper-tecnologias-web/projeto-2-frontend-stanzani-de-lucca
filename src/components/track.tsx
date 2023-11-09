52b275770580c9371a35fa450247e5504f08ee2e
Pedro Stanzani <pedrohstanzani@gmail.com>
2023-11-09 07:42:22 -0300

    Update Track component

--- src/components/track.tsx
+++ src/components/track.tsx
@@ -13,17 +13,17 @@ const Track = ({
   return (
     <div className="flex cursor-pointer flex-row items-center gap-2.5 rounded-md bg-neutral-900 px-3.5 py-2.5 transition-colors hover:bg-[#1B1B1B] md:gap-4">
       {/* <div className="h-11 w-11 min-w-[2.75rem] rounded-md bg-white"></div> */}
-      <p className="text-sm text-neutral-300">{songId.toString()}</p>
+      <p className="text-xs text-neutral-300">{songId.toString()}</p>
       <img
         className="2-9 h-9 min-w-[2.25rem] rounded-sm opacity-80"
         src={artworkSrc ? artworkSrc.url : ""}
         alt={`${track.album.name} artwork.`}
       />
       <div className="flex w-full flex-col overflow-hidden pr-5 sm:pr-12 md:flex-row md:items-center">
-        <p className="truncate whitespace-nowrap font-medium md:basis-2/3 md:pr-5">
+        <p className="truncate whitespace-nowrap text-[15px] md:basis-2/3 md:pr-5">
           {track.name}
         </p>
-        <p className="truncate text-sm font-light tracking-tight text-neutral-300 md:basis-1/3">
+        <p className="truncate text-xs font-light tracking-tight text-neutral-300 md:basis-1/3">
           {track.artists[0].name}
         </p>
       </div>
